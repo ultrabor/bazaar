ed11fef9ec334d06261f4084485cc091564c2160
Ismoiljon Olimjonov <slwpoyninza32@gmail.com>
2026-09-19 18:15:15 +0500

    added some changes

--- internal/platform/worker/pool.go
+++ internal/platform/worker/pool.go
@@ -3,20 +3,34 @@ package worker
 import (
 	"context"
 	"log/slog"
+	"sync"
 )
 
 type Job func(ctx context.Context) error
 
 type Pool struct {
-	ctx     context.Context
+	ctx context.Context
+	log *slog.Logger
+	mux sync.Mutex
+	wg  sync.WaitGroup
+
 	workers int
 	jobs    chan Job
-	log     *slog.Logger
+	maxW    int
 }
 
-func New(ctx context.Context, maxWorkers int, log *slog.Logger) *Pool {
+func New(ctx context.Context, maxWorkers int, log *slog.Logger, maxW int) *Pool {
 	log.Info("pool created successfully")
-	return &Pool{ctx: ctx, workers: 0, jobs: make(chan Job, maxWorkers), log: log}
+	return &Pool{
+		ctx:     ctx,
+		log:     log,
+		workers: maxWorkers,
+		jobs:    make(chan Job),
+		maxW:    maxW,
+		wg:      sync.WaitGroup{},
+		mux:     sync.Mutex{},
+	}
+
 }
 
 func (p *Pool) Close() {
@@ -35,17 +49,48 @@ func (p *Pool) Add(job Job) {
 }
 
 func (p *Pool) worker() {
-	go func() {
+
+	for job := range p.jobs {
 		select {
 		case <-p.ctx.Done():
 			p.log.Error("context done")
 			return
-		case job := <-p.jobs:
-			err := job(p.ctx)
-			if err != nil {
-				p.log.Error("worker error", slog.Any("err", err))
-				return
-			}
+		default:
+			p.wg.Add(1)
+			p.mux.Lock()
+			p.workers--
+			p.mux.Unlock()
+			go func(j Job) {
+				defer p.wg.Done()
+
+				err := j
+				if err != nil {
+					p.log.Error("job fail", slog.Any("err", err))
+					return
+				}
+				p.mux.Lock()
+				p.workers++
+				p.mux.Unlock()
+			}(job)
 		}
-	}()
+	}
+
+	p.wg.Wait()
+
+	// go func() {
+	// 	select {
+	// 	case <-p.ctx.Done():
+	// 		p.log.Error("context done")
+	// 		return
+	// 	case job := <-p.jobs:
+	// 		for {
+	// 			go func(ctx context.Context) {
+	// 				err := job(p.ctx)
+	// 				if err != nil {
+	// 					p.log.Info("")
+	// 				}
+	// 			}(p.ctx)
+	// 		}
+	// 	}
+	// }()
 }
