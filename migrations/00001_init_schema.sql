37d2e3953ee472437977eff74c0a932c39899976
Ismoiljon Olimjonov <slwpoyninza32@gmail.com>
2026-09-24 22:03:49 +0500

    added first migrations

--- migrations/00001_init_schema.sql
+++ migrations/00001_init_schema.sql
@@ -1,11 +1,31 @@
 -- migrations/00001_init_schema.sql
 
 -- +goose Up
--- +goose StatementBegin
+CREATE TABLE companies (
+    id UUID PRIMARY KEY,
+    label TEXT NOT NULL,
+    address TEXT,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
 
--- +goose StatementEnd
+CREATE TABLE locations (
+    id UUID PRIMARY KEY,
+    company_id UUID NOT NULL REFERENCES companies(id),
+    type TEXT NOT NULL CHECK (type IN ('warehouse', 'store')),
+    name TEXT NOT NULL,
+    address TEXT,
+    archived BOOLEAN NOT NULL DEFAULT false,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE INDEX locations_company_id_idx ON locations(company_id);
+
+CREATE UNIQUE INDEX locations_unique_active_name_idx
+ON locations (company_id, type, name)
+WHERE archived = false;
 
 -- +goose Down
--- +goose StatementBegin
-
--- +goose StatementEnd
+DROP TABLE locations;
+DROP TABLE companies;
